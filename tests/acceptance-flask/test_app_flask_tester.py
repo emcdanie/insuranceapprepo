11476624fa26412f5efcf11c44101235d7651c0c
Elleta <elleta.mcdaniel@allianz.es>
2022-07-26 11:58:27 +0200

    final

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -16,11 +16,43 @@ class TestApp(unittest.TestCase):
         
     def test_2_get_restricted(self):
         response = self.client.get('/api/v1/users/', content_type='application/json', headers=self.headers)
-        self.assertEqual(response.status_code, 200)
         data = response.json
         print(f"get restricted: {data}")
         self.assertLess(0, len(data))
         self.assertIn("address", data[0])
-        self.assertIn("B Street 1", data[0]["address"])
         
+    
+    def test_2_post(self):
+        test_data = {"address": "C Street 1",
+                     "email": "jd@myinsuranceapp.com",
+                     "fullname": "John Doe",
+                     "birthdate": "02.10.2020",
+                     "country": "Berlin",
+                     "city": "Spain",
+                     "password": "poo",}
+        response = self.client.post('/api/v1/users/',content_type='application/json',headers=self.headers, json=test_data)
+        data=response.json
+        print(f"post: {data}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data["ok"])
 
+    def test_3_get_none(self):
+        response =self.client.post('/api/v1/token', content_type='application/json')
+        data=response.json
+        print(f"get post 2: {data}")
+        self.assertEqual(response.status_code, 400)
+        
+    def test_4_post_emailmissing(self):
+        test_data = {"address": "C Street 1",
+                     "fullname": "John Doe",
+                     "birthdate": "02.10.2020",
+                     "country": "Berlin",
+                     "city": "Spain",
+                     "password": "poo",}
+        response = self.client.post('/api/v1/users/',content_type='application/json',headers=self.headers, json=test_data)
+        data=response.json
+        print(f"post: {data}")
+        self.assertEqual(response.status_code, 500)
+       
+
+   
